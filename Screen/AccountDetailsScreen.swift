7fcfddddac40ea57d54bada71532671e5e68a588
alpeshvmodi <alpeshvmodi@gmail.com>
2023-08-09 18:38:09 +0530

    Account details basic page

--- Screen/AccountDetailsScreen.swift
+++ Screen/AccountDetailsScreen.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct AccountDetailsScreen: View {
-    var accountId: Account.ID
+    var accountId: String
+    var accountName: String
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        VStack{
-            Text("Pushed the view in into the stack with AccountID - \(accountId)")
-                .navigationBarBackButtonHidden(true)
-                .navigationBarItems(leading: backButton)
+        VStack(spacing: 20) {
+            
+            AccountDetailsHeaderComp(accountId: accountId, accountName: accountName)
+            
+            // Contact list component
+            
+            NotesListComp()
+            
+            HStack {
+                Text("Pushed the view into the stack with AccountID - \(accountId)----\(accountName)")
+            }
+            
+            Spacer()
         }
+        .padding()
+        .background(Color("Background"))
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
     }
     
     private var backButton: some View {
@@ -24,9 +38,83 @@ struct AccountDetailsScreen: View {
             // This will dismiss the AccountDetailsScreen and go back to the previous view
             self.presentationMode.wrappedValue.dismiss()
         }) {
-            Image(systemName: "chevron.left")
-                .font(.title2)
+            HStack {
+                Image(systemName: "arrow.left")
+                Text("Details")
+            }
+            .foregroundColor(Color("SaveButtonBackground"))
         }
     }
 }
 
+struct AccountDetailsHeaderComp: View {
+    var accountId: String
+    var accountName: String
+    
+    var body: some View {
+        HStack{
+            Image("Buildings")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 25.0, height: 25.0)
+            
+            Text("Account Details")
+            
+            Spacer()
+        }
+        VStack(spacing: 5) {
+            HStack {
+                Text("ACCOUNT")
+                    .font(.custom("Nunito-Regular",size: 14))
+                Spacer()
+            }
+            HStack {
+                Text(accountName)
+                    .font(.custom("Nunito-Regular",size: 22))
+                Spacer()
+            }
+        }
+        .padding()
+        .background(.white)
+        .cornerRadius(5) /// make the background rounded
+        .overlay( /// apply a rounded border
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color("BorderColor"), lineWidth: 1)
+        )
+    }
+}
+
+struct NotesListComp: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            HStack{
+                Image("NoteIcon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 20.0, height: 20.0)
+                
+                Text("Notes")
+                
+                Spacer()
+                
+                Image("CreateNoteIcon")
+                    .frame(width: 20.0, height: 20.0)
+            }
+            VStack(spacing: 5) {
+                HStack {
+                    Spacer()
+                    Text("Add notes and sync with your salesforce account")
+                        .font(.custom("Nunito-Regular",size: 12))
+                    Spacer()
+                }
+            }
+            .padding()
+            .cornerRadius(5) /// make the background rounded
+            .overlay( /// apply a rounded border
+                RoundedRectangle(cornerRadius: 5)
+                //                    .stroke(Color("BorderColor"), lineWidth: 1)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [1,5]))
+            )
+        }
+    }
+}
